dbc250b0eb5ae66915a8d19d9d7d25c24cf74701
dilipdeva <dilip.devakumar3@gmail.com>
2023-04-06 13:58:58 +0530

    final version 1.o

--- src/main/java/Controller/OrderController.java
+++ src/main/java/Controller/OrderController.java
@@ -7,6 +7,7 @@ package Controller;
 import DBConnection.DBConnection;
 import Models.Orders;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
@@ -32,6 +33,39 @@ public class OrderController {
             System.out.println("ERROR"+" "+ex);
         }
          
+        return false;
+    }
+    
+    public boolean orderUpdate(Orders order) {
+        
+         Connection connection = DBConnection.getConnection(); 
+         try {   
+            Statement statement = connection.createStatement();
+            int rows = statement.executeUpdate("UPDATE `order` SET  `name` = '"
+                    + order.getOrder_name() +  "', `birthday` = '" + order.getOrder_birthday() +"', `email` = '" + order.getOrder_email() + "', `loan` = '" + order.getOrder_loan() + "', `plan` = '" + order.getOrder_plan() + "', `number` = '" + order.getOrder_number() + "', `price` = '" + order.getOrder_price() + "' WHERE (`id` = '"  + order.getOrder_id() + "')");
+            
+            return rows > 0;
+            
+        } catch (Exception ex){
+            System.out.println("ERROR"+" "+ex);
+        }  
+        return false;
+     }
+    
+    public boolean deleteOrder(String id){
+    Connection connection = DBConnection.getConnection(); 
+     
+        try{
+          Statement stmt=connection.createStatement();
+           
+          int rows=stmt.executeUpdate(" DELETE FROM `order` WHERE "
+           + "(`id`='"+id+"');");
+      		
+	    return rows>0;
+	
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
         return false;
     }
 }
--- src/main/java/Services/OrderService.java
+++ src/main/java/Services/OrderService.java
@@ -24,6 +24,17 @@ public class OrderService {
     public boolean Cus_ord(@WebParam(name="order")Orders order) {
         OrderController ord_controller = new OrderController();
         return ord_controller.customerOrder(order);
-       
+    }
+    
+     @WebMethod(operationName = "deleteOrder")
+    public boolean deleteOrder(@WebParam(name="id") String id){
+    OrderController ord_controller = new OrderController();
+    return ord_controller.deleteOrder(id);
+    }
+    
+    @WebMethod(operationName = "updateOrders")
+    public boolean updateOrders(@WebParam(name="order")Orders order) {
+    OrderController cus_update = new OrderController();
+    return cus_update.orderUpdate(order);
     }
 }
